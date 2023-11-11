46b845adeb5a7ebe73f89372a2598014237ced6b
Brinth KR <itzbrinth@gmail.com>
2023-11-11 10:47:18 +0530

    adds Root

---
+++ src/Root.js
@@ -0,0 +1,68 @@
+import { Routes, Route } from "react-router-dom";
+import Button from './examples/Button'
+import Grid from './examples/Grid'
+import Layout from './examples/Layout';
+import BreadcrumbExamples from './examples/Breadcrumb';
+import DropdownExamples from './examples/Dropdown';
+import Menu from './examples/Menu';
+import PaginationExample from './examples/Pagination';
+import StepsExample from './examples/Steps';
+import AutoCompleteExamples from './examples/AutoComplete';
+import SelectExamples from './examples/Select';
+import RateExamples from './examples/Rate';
+import SliderExamples from './examples/Slider';
+import CarouselExamples from './examples/Carousel';
+import CollapseExamples from './examples/Collapse';
+import TabsExamples from './examples/Tabs';
+import ModalExamples from './examples/Modal';
+import ProgressExamples from './examples/Progress';
+import DrawerExamples from './examples/Drawer';
+import TooltipExamples from './examples/Tooltip';
+import NotificationExamples from './examples/Notification';
+import TimelineExamples from './examples/Timeline';
+import FileExplorerExamples from './examples/FileExplorer';
+import InfiniteScrollerExamples from './examples/InfiniteScroller';
+import StopwatchExamples from './examples/Stopwatch';
+import TimerExamples from './examples/Timer';
+import CommentBoxExample from './examples/CommentBox';
+import CalendarExample from './examples/Calendar';
+import TictacktoeExamples from './examples/Tictacktoe';
+import App from './App';
+
+function Root() {
+    return (
+        <Routes>
+            <Route path="/button" element={<Button />} />
+            <Route path="/breadcrumb" element={<BreadcrumbExamples />} />
+            <Route path="/dropdown" element={<DropdownExamples />} />
+            <Route path="/layout" element={<Layout />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/pagination" element={<PaginationExample />} />
+            <Route path="/steps" element={<StepsExample />} />
+            <Route path="/autocomplete" element={<AutoCompleteExamples />} />
+            <Route path="/select" element={<SelectExamples />} />
+            <Route path="/rate" element={<RateExamples />} />
+            <Route path="/slider" element={<SliderExamples />} />
+            <Route path="/carousel" element={<CarouselExamples />} />
+            <Route path="/collapse" element={<CollapseExamples />} />
+            <Route path="/tabs" element={<TabsExamples />} />
+            <Route path="/modal" element={<ModalExamples />} />
+            <Route path="/progress" element={<ProgressExamples />} />
+            <Route path="/drawer" element={<DrawerExamples />} />
+            <Route path="/tooltip" element={<TooltipExamples />} />
+            <Route path="/notification" element={<NotificationExamples />} />
+            <Route path="/timeline" element={<TimelineExamples />} />
+            <Route path="/file-explorer" element={<FileExplorerExamples />} />
+            <Route path="/infinite-scroller" element={<InfiniteScrollerExamples />} />
+            <Route path="/stopwatch" element={<StopwatchExamples />} />
+            <Route path="/timer" element={<TimerExamples />} />
+            <Route path="/comment-box" element={<CommentBoxExample />} />
+            <Route path="/calendar" element={<CalendarExample />} />
+            <Route path='/tictacktoe' element={<TictacktoeExamples />} />
+            <Route path='/grid' element={<Grid />} />
+            <Route path="/" element={<App />} />
+        </Routes>
+    );
+}
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './App';
+import Root from './Root';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
